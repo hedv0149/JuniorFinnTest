429087a6fe9e148652c9fbf03cd72c06c31d6b65
Hedviga <hedv0149@edu.eal.dk>
2018-04-11 19:09:30 +0200

    App added

---
+++ JuniorFINNApp/JuniorFINNApp/JuniorFINNApp/Ads.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JuniorFINNApp
+{
+    public class Ads
+    {
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+
+        public List<Ads> AllAdsList = new List<Ads>();
+
+       
+        public void ConvertJSONDataIntoAds()
+        {
+            string uri = "https://gist.githubusercontent.com/3lvis/3799feea005ed49942dcb56386ecec2b/raw/63249144485884d279d55f4f3907e37098f55c74/discover.json";
+            var json = new WebClient().DownloadString(uri);
+            JsonConvert.DeserializeObject<List<Ads>>(json);
+        }
+
+
+    }
+
+
+}
---
+++ JuniorFINNApp/JuniorFINNApp/JuniorFINNApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JuniorFINNApp
+{
+	public partial class MainPage : ContentPage
+	{
+        Ads ads = new Ads();
+        List<Label> labelList = new List<Label>();
+        Grid GridForPuttingDataIntoTwoColumns;
+        int countOfRows = 0;
+        public MainPage()
+		{
+			InitializeComponent();
+            ads.ConvertJSONDataIntoAds();
+            GridForPuttingDataIntoTwoColumns = this.FindByName<Grid>("GridForPuttingDataIntoTwoColumnss");
+            PopulateList();
+
+            for (int i = 0; i < labelList.Count; i=+2)
+            {
+                
+                GridForPuttingDataIntoTwoColumns.Children.Add(labelList[i], countOfRows /*col*/, 0 /*row*/);
+                GridForPuttingDataIntoTwoColumns.Children.Add(labelList[i++], countOfRows, 1);
+                countOfRows++;
+            }
+
+        }
+
+        private void PopulateList()
+        {
+            var topLeft = new Label { Text = "Top Left" };
+               var topRight = new Label { Text = "Top Right" };
+              //var bottomLeft = new Label { Text = "Bottom Left" };
+                //var bottomRight = new Label { Text = "Bottom Right" };
+
+            labelList.Add(topLeft);
+               labelList.Add(topRight);
+               //labelList.Add(bottomLeft);
+                //labelList.Add(bottomRight);
+        }
+    }
+}
